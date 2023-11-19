c9f62de08cdf560f8feaa2f30230a4ad2c5dde3a
Didrik147 <didrik.roest@yahoo.com>
2023-11-19 12:57:56 +0100

    lesson 21

--- react-firebase/my-first-react-app/src/App-21.js
+++ react-firebase/my-first-react-app/src/App-21.js
@@ -14,20 +14,17 @@ function App() {
   ])
   
   const handleClick = () => {
-    //name = 'Luigi'
-    setName('Luigi')
-    
-    console.log(name)
+
   }
+
+  /* Currently at 0:54 */
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick = {handleClick}>Change name</button>
-
       { events.map((event, index) => (
         <div key={event.id}>
           <h2>{index} - {event.title}</h2>
+          <button>Delete Event</button>
         </div>
       )) }
     </div>
