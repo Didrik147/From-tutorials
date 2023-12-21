14cc1fdbe0867dae85e347b99402c24f5c31ee04
Didrik147 <didrik.roest@yahoo.com>
2023-12-21 11:53:52 +0100

    le-53

--- react-firebase/tripago/src/hooks/useFetch.js
+++ react-firebase/tripago/src/hooks/useFetch.js
@@ -2,11 +2,17 @@ import { useState, useEffect } from 'react'
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsPending(true)
+
       const res = await fetch(url)
       const json = await res.json()
+      
+      setIsPending(false)
+      
       setData(json)
     }
 
@@ -14,5 +20,5 @@ export const useFetch = (url) => {
 
   }, [url])
 
-  return { data: data }
+  return { data, isPending }
 }
